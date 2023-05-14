1837f019c37f7d813e3921cda4822b21513556d4
Birte <78079178+BiBa-01@users.noreply.github.com>
2023-05-14 10:00:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def predict():
 
     try:
         # Load pretrained model as clf. Try any one model. 
-         clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
+        clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
         # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
        # clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
     except:
